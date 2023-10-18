25fb41bc538de8cea3e2e36370545e024ee3aca1
Gustavo Morais <gustvmorais@gmail.com>
2023-10-18 09:41:48 -0300

    Deixando o conteudo dinamico

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,14 @@
+export const dataFake = [
+  {
+    "id" : "1",
+    "image": "https://pngimg.com/d/rick_morty_PNG11.png",
+    "title": "Minha Noticia",
+    "description": "Rick and Morty are back and sounding more like themselves than ever, it reads. Its season seven, and the possibilities are endless: whats up with Jerry? EVIL Summer?! And will they ever go back to the high school?! Maybe not! But lets find out! Theres probably less piss than last season. Rick and Morty, 100 years! Or at least until season 10!"
+  },
+  {
+    "id" : "2",
+    "image": "ssss",
+    "title": "Rick and Morty",
+    "description": "Aolsa"
+  }
+]
