11abeead1e77cac53d06e47c6c654881ed755da2
Gustavo Morais <gustvmorais@gmail.com>
2023-10-18 09:56:27 -0300

    Finalizando componentes

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -2,13 +2,25 @@ export const dataFake = [
   {
     "id" : "1",
     "image": "https://pngimg.com/d/rick_morty_PNG11.png",
-    "title": "Minha Noticia",
+    "title": "New Season is impressive",
     "description": "Rick and Morty are back and sounding more like themselves than ever, it reads. Its season seven, and the possibilities are endless: whats up with Jerry? EVIL Summer?! And will they ever go back to the high school?! Maybe not! But lets find out! Theres probably less piss than last season. Rick and Morty, 100 years! Or at least until season 10!"
   },
   {
     "id" : "2",
-    "image": "ssss",
+    "image": "https://ovicio.com.br/wp-content/uploads/2023/06/20230614-ovicio-rick-morty-season-7.jpg",
     "title": "Rick and Morty",
-    "description": "Aolsa"
+    "description": "Being the co-creator and star of the show, it's hard to deny that Justin Roiland was a major component of Rick and Morty's success. However, in their statement following Roiland's firing, Adult Swim made it explicitly clear that Roiland's firing did not mean the end of the beloved show, stating, Rick and Morty will continue. The talented and dedicated crew are hard at work on Season 7. To learn more about the upcoming season's new cast, episode count, plot, and more, here is everything we know so far about Rick and Morty Season 7"
+  },
+  {
+    "id" : "3",
+    "image": "https://images.thedirect.com/media/article_full/rick-mortys.jpg",
+    "title": "Rick and Morty voices",
+    "description": "Up until now, mad scientist Rick and his grandson Morty were voiced by the same actor. But he's been replaced, giving both characters a new sound. The release of the series trailer last month had left fans wondering whose voices they were hearing. Now they know it's Ian Cardoni as Rick and Harry Belden as Morty"
+  },
+  {
+    "id" : "4",
+    "image": "https://www.rollingstone.com/wp-content/uploads/2022/08/22-RAM-S6-KEY-ART-1080x1080-1-e1661990077221.png?w=885",
+    "title": "Rick and Morty suprise",
+    "description": "Rick and Morty season 7, episode 1 brings back five characters from the show's past, and here's why they're here and what they've been up to since they were last seen. Rick and Morty's cast of side characters has grown into a massive pool of iconic supporting cast members, with many one-off gag appearances getting turned into fully fleshed-out characters. Luckily, Rick and Morty isn't done developing some of them yet. Five of the most popular side characters have surprise returns in Rick and Morty season 7, episode 1, with all of them playing a major role in the main story of the season 7 premiere"
   }
 ]
